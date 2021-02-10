861e6ab3cdc12dad9b4fbb62c2d4c10bbecd451a
Hidayat <hidayat@MacBook-Air.local>
2021-02-10 20:28:25 +0400

    initial commit

--- src/containers/TimeManagement/TimeManager/TimeManager.js
+++ src/containers/TimeManagement/TimeManager/TimeManager.js
@@ -1,13 +1,29 @@
 import React from "react";
 
+import { Input } from "antd";
 import { DatePicker, Space } from "antd";
+import { Select } from "antd";
+
 const { RangePicker } = DatePicker;
+
+const { Option } = Select;
+
+const children = [];
+for (let i = 10; i < 36; i++) {
+  children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+}
 
 function TimeManager(props) {
   return (
-    <Space direction="vertical" size={12}>
-      <RangePicker showTime={{ format: "HH:mm" }} format="YYYY-MM-DD HH:mm" />
-    </Space>
+    <div>
+      <Space direction="horizontal" size={12}>
+        <Input placeholder="Enter work name" style={{ width: "300px" }} />
+        <Select mode="tags" style={{ width: "150px" }} placeholder="Tags">
+          {children}
+        </Select>
+        <RangePicker showTime={{ format: "HH:mm" }} format="YYYY-MM-DD HH:mm" />
+      </Space>
+    </div>
   );
 }
 
